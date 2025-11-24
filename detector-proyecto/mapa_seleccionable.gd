aa7e25713efbc2ee02e4bc9ad1e3651fae8b940d
DAGimenez <gimenezdylan753@gmail.com>
2025-11-24 12:37:45 -0300

    pequeños cambios

--- detector-proyecto/mapa_seleccionable.gd
+++ detector-proyecto/mapa_seleccionable.gd
@@ -1,11 +1,16 @@
 extends Control
-
+const Provincia_1 := ("res://Escenas/provincia_1.tscn")
+const Provincia_2 := ("res://Escenas/provincia_2.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _on_provincia2_pressed() -> void:
 	
-	get_tree().change_scene_to_file("res://provincia_1.tscn")
+	get_tree().change_scene_to_file("res://Escenas/provincia_1.tscn")
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://provincia_1.tscn")
+	get_tree().change_scene_to_file(Provincia_1)
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file(Provincia_2)
