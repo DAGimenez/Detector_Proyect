4f6d29e105354e2ce1b1bf3916cecebe6b0862e2
kevinMG01 <montanokevin46@gmail.com>
2026-02-03 10:28:21 -0300

    new

--- detector-proyecto/Escenas/ui/control_interfaz.gd
+++ detector-proyecto/Escenas/ui/control_interfaz.gd
@@ -1,33 +1,57 @@
 extends CanvasLayer
 
-
-@onready var hojas = [
+@onready var hojas : Array[Control] = [
 	$Control/menu,
 	$Control/misiones_mapa,
 	$Control/mochila
 ]
 
+var offsets := [
+	Vector2(0, 0),
+	Vector2(12, 6),
+	Vector2(24, 12)
+]
 
-var pagina_actual : int = 1
+var front_pos : Vector2
+var tweens := {}
 
-func actualizar_hojas():
+
+func _ready():
+	front_pos = hojas[0].position
+	actualizar()
+
+
+func actualizar():
 	for i in hojas.size():
-		var hoja = hojas[i]
-		var distancia = abs(i - pagina_actual)
-		
-		hoja.z_index = -distancia
-		
-		# Corrimiento
-		hoja.position = Vector2(
-			distancia * 12,
-			distancia * 6
-		)
+		var hoja := hojas[i]
+
+		hoja.z_index = -i
+		var target = front_pos + offsets[i]
+
+		# Matamos tween anterior si existe
+		if tweens.has(hoja):
+			tweens[hoja].kill()
+
+		var tween := create_tween()
+		tween.set_trans(Tween.TRANS_QUAD)
+		tween.set_ease(Tween.EASE_OUT)
+		tween.tween_property(hoja, "position", target, 0.0)
+
+		tweens[hoja] = tween
+
+
+func siguiente():
+	hojas.push_back(hojas.pop_front())
+	actualizar()
+
+
+func anterior():
+	hojas.push_front(hojas.pop_back())
+	actualizar()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("vi_e"):
-		pagina_actual = clamp(pagina_actual + 1, 0, hojas.size() - 1)
-		actualizar_hojas()
+		siguiente()
 	
 	if event.is_action_pressed("vi_q"):
-		pagina_actual = clamp(pagina_actual - 1, 0, hojas.size() - 1)
-		actualizar_hojas()
+		anterior()
