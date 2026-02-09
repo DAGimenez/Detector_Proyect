64fd4e142e27fc629dfbec85861ba4459e8928c8
kevinMG01 <112394787+kevinMG01@users.noreply.github.com>
2026-02-09 17:28:36 -0300

    ui funcional

--- detector-proyecto/Escenas/ui/control_interfaz.gd
+++ detector-proyecto/Escenas/ui/control_interfaz.gd
@@ -1,56 +1,65 @@
 extends CanvasLayer
 
 @onready var hojas : Array[Control] = [
-	$Control/menu,
+	$Control/mochila,
 	$Control/misiones_mapa,
-	$Control/mochila
+	$Control/menu,
 ]
 
-var offsets := [
-	Vector2(0, 0),
-	Vector2(12, 6),
-	Vector2(24, 12)
-]
-
-var front_pos : Vector2
-var tweens := {}
+var posiciones : Array[Vector2] = []
+var moviendo : bool = false
 
 
 func _ready():
-	front_pos = hojas[0].position
-	actualizar()
+	for h in hojas:
+		posiciones.append(h.position)
+	actualizar_visual()
+	pass
 
 
+func actualizar_visual():
+	for i in range(hojas.size()):
+		hojas[i].z_index = -i
 func actualizar():
-	for i in hojas.size():
+	for i in range(hojas.size()):
 		var hoja := hojas[i]
 
 		hoja.z_index = -i
-		var target = front_pos + offsets[i]
+	
 
-		# Matamos tween anterior si existe
-		if tweens.has(hoja):
-			tweens[hoja].kill()
-
-		var tween := create_tween()
-		tween.set_trans(Tween.TRANS_QUAD)
-		tween.set_ease(Tween.EASE_OUT)
-		tween.tween_property(hoja, "position", target, 0.0)
-
-		tweens[hoja] = tween
 
 
 func siguiente():
-	hojas.push_back(hojas.pop_front())
-	actualizar()
+	if moviendo: return
+	moviendo = true
+	var frontal = hojas[0]
+	var pos_final = posiciones[hojas.size() - 1]
+	var tween = create_tween().set_parallel(false)
+	tween.tween_property(frontal, "position", frontal.position + Vector2(200, 0), 0.2).set_trans(Tween.TRANS_CUBIC)
+	
+	for i in range(1, hojas.size()):
+		var sig_tween = create_tween().set_parallel(true)
+		sig_tween.tween_property(hojas[i], "position", posiciones[i-1], 0.3).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 
+	tween.chain().step_finished # Espera a que termine de salir
+	
+	var hoja_frontal = hojas.pop_front()
+	hojas.push_back(hoja_frontal)
+	actualizar_visual()
+	
+	var vuelta = create_tween()
+	vuelta.tween_property(hoja_frontal, "position", pos_final, 0.2).set_trans(Tween.TRANS_CUBIC)
+	
+	await vuelta.finished
+	moviendo = false
 
 func anterior():
-	hojas.push_front(hojas.pop_back())
+	var hoja_fondo = hojas.pop_back()
+	hojas.push_front(hoja_fondo)
 	actualizar()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("vi_e"):
+	if event.is_action_pressed("vi_e") and not moviendo:
 		siguiente()
 	
 	if event.is_action_pressed("vi_q"):
