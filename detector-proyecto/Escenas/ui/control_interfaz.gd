8fbd08c0ab2b2284dae43fc527c30a7ed5f5f0f9
kevinMG01 <112394787+kevinMG01@users.noreply.github.com>
2026-02-09 19:41:31 -0300

    mochila con objetos

--- detector-proyecto/Escenas/ui/control_interfaz.gd
+++ detector-proyecto/Escenas/ui/control_interfaz.gd
@@ -13,9 +13,16 @@ var moviendo : bool = false
 func _ready():
 	for h in hojas:
 		posiciones.append(h.position)
-	actualizar_visual()
-	pass
+	reorganizar(false)
 
+
+func reorganizar(animar: bool):
+	for i in range(hojas.size()):
+		hojas[i].z_index = -i # La primera (0) siempre arriba
+		if animar:
+			create_tween().tween_property(hojas[i], "position", posiciones[i], 0.25).set_trans(Tween.TRANS_SINE)
+		else:
+			hojas[i].position = posiciones[i]
 
 func actualizar_visual():
 	for i in range(hojas.size()):
@@ -32,35 +39,50 @@ func actualizar():
 func siguiente():
 	if moviendo: return
 	moviendo = true
+	
 	var frontal = hojas[0]
-	var pos_final = posiciones[hojas.size() - 1]
-	var tween = create_tween().set_parallel(false)
-	tween.tween_property(frontal, "position", frontal.position + Vector2(200, 0), 0.2).set_trans(Tween.TRANS_CUBIC)
+	var tween = create_tween()
 	
-	for i in range(1, hojas.size()):
-		var sig_tween = create_tween().set_parallel(true)
-		sig_tween.tween_property(hojas[i], "position", posiciones[i-1], 0.3).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	await tween.tween_property(frontal, "position", frontal.position + Vector2(250, 0), 0.15).finished
 
-	tween.chain().step_finished # Espera a que termine de salir
+	hojas.push_back(hojas.pop_front())
 	
-	var hoja_frontal = hojas.pop_front()
-	hojas.push_back(hoja_frontal)
-	actualizar_visual()
-	
-	var vuelta = create_tween()
-	vuelta.tween_property(hoja_frontal, "position", pos_final, 0.2).set_trans(Tween.TRANS_CUBIC)
-	
-	await vuelta.finished
+	reorganizar(true)
+	await get_tree().create_timer(0.2).timeout
 	moviendo = false
-
 func anterior():
-	var hoja_fondo = hojas.pop_back()
-	hojas.push_front(hoja_fondo)
-	actualizar()
+	if moviendo: return
+	moviendo = true
+	
+	var fondo = hojas.back()
+	var tween = create_tween()
 
+	await tween.tween_property(fondo, "position", fondo.position + Vector2(250, 0), 0.15).finished
+
+	hojas.push_front(hojas.pop_back())
+	
+	reorganizar(true)
+	await get_tree().create_timer(0.2).timeout
+	moviendo = false
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("vi_e") and not moviendo:
+	if event.is_action_pressed("vi_e"):
 		siguiente()
 	
 	if event.is_action_pressed("vi_q"):
 		anterior()
+
+func ir_a_hoja(objetivo: Control):
+	if moviendo or hojas[0] == objetivo: return
+	var indice_objetivo = hojas.find(objetivo)
+	
+	for i in range(indice_objetivo):
+		await siguiente()
+
+func _on_but_1_button_down() -> void:
+	ir_a_hoja($Control/mochila)
+
+func _on_but_2_button_down() -> void:
+	ir_a_hoja($Control/misiones_mapa)
+
+func _on_but_3_button_down() -> void:
+	ir_a_hoja($Control/menu)
---
+++ detector-proyecto/Escenas/ui/ui.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var objetos_inmovible = preload("res://Escenas/inventario/objetos_inmovibles/objetos_inmovibles.tscn")
+@onready var objetos_usables: PanelContainer = $control_Interfaz/Control/mochila/PanelContainer/MarginContainer/MarginContainer/VBoxContainer/objetos_usables
+@onready var objetos_desenterados: PanelContainer = $control_Interfaz/Control/mochila/PanelContainer/MarginContainer/MarginContainer/VBoxContainer/objetos_desenterados
+@onready var grid_desenterados: GridContainer = $control_Interfaz/Control/mochila/PanelContainer/MarginContainer/MarginContainer/VBoxContainer/objetos_desenterados/MarginContainer/Grid_desenterados
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_up"):
+		$control_Interfaz.visible = !$control_Interfaz.visible
+
+func recolectar_objetos(objeto: Resource):
+	var new_objeto_inmovible = objetos_inmovible.instantiate()
+	new_objeto_inmovible.texture = objeto.sprite
+	new_objeto_inmovible.position = objetos_desenterados.position
+	grid_desenterados.add_child(new_objeto_inmovible)
